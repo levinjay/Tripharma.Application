744cfa3fc586d7d67cfff373690fe02daab8401b
Levin jay <levinjay@users.noreply.github.com>
2026-09-21 03:02:55 -0700

    init

---
+++ User/IUser.cs
@@ -0,0 +1,20 @@
+﻿using Tripharma.Core.User;
+using Tripharma.Core.Utility;
+
+namespace Tripharma.Application.User
+{
+    public interface IUserDetail
+    {
+        Task<PagedResponse<UserDetail>> GetPagedAsync(int pageNumber, int pageSize);
+        Task<PagedResponse<UserDetail>> GetPagedForOwnerAsync(string? ownerSubject, long? ownerNumericId, int pageNumber, int pageSize);
+        Task<UserDetail?> GetForOwnerAsync(string? ownerSubject, long? ownerNumericId);
+        Task<UserDetail?> GetByIdAsync(string id);
+        Task<UserDetail?> GetByIdForOwnerAsync(string id, string? ownerSubject, long? ownerNumericId);
+        Task<UserDetail> CreateAsync(UserDetail user);
+        Task<UserDetail> CreateAsync(UserDetail user, string? ownerSubject, long? ownerNumericId);
+        Task<bool> UpdateAsync(UserDetail user);
+        Task<bool> UpdateAsync(UserDetail user, string? ownerSubject, long? ownerNumericId);
+        Task<bool> DeleteAsync(string id);
+        Task<bool> DeleteAsync(string id, string? ownerSubject, long? ownerNumericId);
+    }
+}
